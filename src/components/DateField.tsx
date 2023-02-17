97e9db99df662035ead60a97295bbac8b7bf271d
bebeji-nappa <masadecade@gmail.com>
2023-02-18 04:54:15 +0900

    Create DatePicker

--- src/components/DateField.tsx
+++ src/components/DateField.tsx
@@ -1,7 +1,27 @@
 import * as React from "react";
-
+import { useMemo } from "react";
+import { currentDate, currentDateTime, DayOfWeeks, Days } from "../modules/data";
 export const DateField = () => {
+  const days = useMemo(() => Days(currentDate), []);
+  console.log(days)
   return (
-    <h1>Date</h1>
+    <table>
+      <thead>
+        <tr>
+          {DayOfWeeks.map((day: string, i: number) => 
+            <th key={`week-${i}`}>{day}</th>
+          )}
+        </tr>
+      </thead>
+      <tbody>
+        {days.map((day, idx) => 
+          <tr key={`days-${idx}`}>
+            {day.map((v, idx) => 
+              <td key={`day-${idx}`}>{v.value}</td>
+            )}
+          </tr>
+        )}
+      </tbody>
+    </table>
   )
 }
---
+++ src/modules/data.ts
@@ -0,0 +1,52 @@
+import dayjs from "dayjs";
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(timezone);
+
+const now = dayjs();
+const date: string = now.format("YYYY-MM-DD");
+const datetime: string = now.format("YYYY-MM-DD hh:mm");
+const dayofweek: string = now.format("d");
+
+export const currentDate: string = date;
+export const currentDateTime: string = datetime;
+export const currentDayOfWeek: string = dayofweek;
+export const DayOfWeeks: string[] = [
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat",
+]
+export const Days = (date: string) => {
+  const dateData = dayjs(date);
+  const data = [];
+  const endOfDay: number = Number(dateData.endOf("month").format("D"));
+  const month = dateData.format("M");
+  const year = dateData.format("YYYY");
+  let arr = Array(7).fill(null);
+  for (let i: number = 1; i <= endOfDay; i++) {
+    const week: number = Number(dayjs(`${year}-${month}-${i}`).format("d"));
+    if (arr.length && week === 0) {
+      data.push(arr);
+      arr = Array(7).fill(null);
+    }
+    if (i === 1 && week > 0) {
+      const prevEndOfDay: number = Number(dayjs(`${year}-${Number(month) - 1}-${i}`).endOf("month").format("D"));
+      arr = arr.map((v, i) => (i < week) && { value: prevEndOfDay - (week - i - 1), month: "prev" });
+    }
+    arr[week] = { value: i, month: "now" };
+    if (i === endOfDay) {
+      arr.forEach((v, i) => {
+        if (i > week) {
+          arr[i] = { value: i - week, month: "next" }
+        } 
+      });
+      data.push(arr);
+    }
+  }
+  console.log(data)
+  return data;
+}
